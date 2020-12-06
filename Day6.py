96d6d4d9915991ad98eedc3cca8d8ccaa286e725
jnchengtarantino <jn@chengtarantino.com>
2020-12-06 00:37:50 -0500

    day 6

--- Day6.py
+++ Day6.py
@@ -1,8 +1,36 @@
+import string
 file = open("day6input.txt")
 lines = file.readlines()
-data = list(map(int,lines))
 
-for i in range(len(data)):
-    pass
+def intersection(lst1, lst2): 
+    lst3 = [value for value in lst1 if value in lst2] 
+    return lst3 
+
+letters_sets =[] 
+count = 0
+group = 0
+for line in lines:
+    if line.strip() == "":
+        letters = letters_sets[0]
+        for person in letters_sets:
+            letters = intersection(letters, person)
+        count += len(letters)
+        print(str(len(letters)) + " for group " +str(group))
+        group +=1
+        letters_sets = []
+    else:
+        letters = []
+        for c in line.strip():
+            letters.append(c)
+        letters_sets.append(letters)
+
+letters = letters_sets[0]
+for person in letters_sets:
+    letters = intersection(letters, person)
+count += len(letters)
+print(str(len(letters)) + " for group " +str(group))
+group +=1
+letters_sets = []
+print(count)
        
 file.close()
