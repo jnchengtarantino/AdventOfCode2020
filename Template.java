bdfe38a81e4c7bd13a34049ae86f71da35a59ed4
jnchengtarantino <jn@chengtarantino.com>
2020-12-07 02:05:38 -0500

    fixes alignment in java template

--- Template.java
+++ Template.java
@@ -8,36 +8,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Template {
-   public static void main(String[] args) {
-    File file = new File("dayXinput.txt");
-    BufferedReader reader = null;
+    public static void main(String[] args) {
+        File file = new File("dayXinput.txt");
+        BufferedReader reader = null;
 
-    try {
-        reader = new BufferedReader(new FileReader(file));
-        String text = null;
-
-        //part 1
-        while ((text = reader.readLine()) != null) {
-            
-        }
-
-        //part 2
-        while ((text = reader.readLine()) != null) {
-            
-        }
-
-        System.out.println("Reached end of file");
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
         try {
-            if (reader != null) {
-                reader.close();
+            reader = new BufferedReader(new FileReader(file));
+            String text = null;
+
+            //part 1
+            while ((text = reader.readLine()) != null) {
+                
             }
+
+            //part 2
+            while ((text = reader.readLine()) != null) {
+                
+            }
+
+            System.out.println("Reached end of file");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+            }
         }
-    }
-   } 
+    } 
 }
