d9762b8b7cf543c6cceba1b225a7bb40d93cf91a
jnchengtarantino <jn@chengtarantino.com>
2020-12-15 09:56:56 -0500

    Day 15, searching dictionaries much faster than iterating lists

---
+++ Day15.py
@@ -0,0 +1,25 @@
+file = open("day15input.txt")
+lines = file.readlines()
+# data = list(map(int,lines))
+
+nums = list(map(int,lines[0].split(",")))
+d = {}
+for j in range(len(nums) - 1): d[nums[j]] = j + 1
+prev = nums[-1]
+i = len(nums) + 1
+while(i < 30000000 + 1):
+    new = None
+    # if 
+    if prev in d:
+        new = i - d[prev] - 1
+    else:
+        new = 0
+    # print(d)
+    # print("turn " + str(i) + " prev " + str(prev) + " new " + str(new))
+    if i % 10000 == 0: print("reached i " + str(i))
+    d[prev] = i - 1
+    prev = new    
+    i += 1
+   
+print(prev)
+file.close()
